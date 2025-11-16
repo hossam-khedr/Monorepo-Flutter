ad7dd65f943561b3130600d46aed7df2516d790a
abdalla shiha <hossamkidr163@gmail.com>
2025-11-16 02:28:40 +0200

    setup core done

---
+++ packages/core/lib/utils/api_call_helper.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:dartz/dartz.dart';
+
+import '../error/error_handler.dart';
+import '../error/faliure.dart';
+import '../network/network_info.dart';
+
+class ApiCallHelper{
+  final NetworkInfoImpl networkInfoImpl = NetworkInfoImpl();
+
+  Future<Either<Failure, T>> safeApiCall<T>({
+    T? input,
+    required Future<T> Function() callback,
+  }) async {
+    try {
+      if (await networkInfoImpl.isConnected) {
+        final result = await callback();
+        return Right(result);
+      } else {
+        return const Left(NetworkFailure('No internet connection'));
+      }
+    } catch (error) {
+      final failure = ErrorHandler.handle(error);
+      return Left(failure);
+    }
+  }
+}
